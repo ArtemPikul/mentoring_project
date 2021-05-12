5da043a5bb9adaed781d492057c07a92c9780733
Artem Pikul <pikul.a@outlook.com>
2021-05-12 21:58:14 +0300

    Refactored waiters into FluentWait

--- src/main/java/core/Configuration.java
+++ src/main/java/core/Configuration.java
@@ -1,10 +1,13 @@
 package core;
 
+import java.time.Duration;
+
 public class Configuration {
 
     public static final String EMAIL_ADDRESS = "mentoring.test.email@gmail.com";
     public static final String EMAIL_PASSWORD = "Hard_Passw0rd";
 
-    public static final long WAIT_TIMEOUT = 15;
+    public static final Duration WAIT_TIMEOUT = Duration.ofSeconds(15);
+    public static final Duration WAIT_POLLING_INTERVAL = Duration.ofSeconds(3);
 
 }
--- src/main/java/driver/WaitUtils.java
+++ src/main/java/driver/WaitUtils.java
@@ -1,28 +1,33 @@
 package driver;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
 
+import java.util.NoSuchElementException;
+
+import static core.Configuration.WAIT_POLLING_INTERVAL;
 import static core.Configuration.WAIT_TIMEOUT;
 import static driver.DriverUtils.getDriver;
 
 public class WaitUtils {
 
-    public static void waitForPageReadyState() {
-        new WebDriverWait(getDriver(), WAIT_TIMEOUT).until(
-                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
-    }
-
     public static void waitForVisibilityOfElementLocated(By locator) {
-        WebDriverWait wait = new WebDriverWait(getDriver(), WAIT_TIMEOUT);
+        FluentWait wait = new FluentWait<>(getDriver())
+                .withTimeout(WAIT_TIMEOUT)
+                .pollingEvery(WAIT_POLLING_INTERVAL)
+                .ignoring(NoSuchElementException.class, ElementNotVisibleException.class);
+
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     public static void waitForElementToBeExpanded(By locator) {
-        WebDriverWait wait = new WebDriverWait(getDriver(), WAIT_TIMEOUT);
+        FluentWait wait = new FluentWait<>(getDriver())
+                .withTimeout(WAIT_TIMEOUT)
+                .pollingEvery(WAIT_POLLING_INTERVAL)
+                .ignoring(NoSuchElementException.class);
+
         wait.until(ExpectedConditions.attributeToBe(locator, "aria-expanded", "true"));
     }
-
 }
